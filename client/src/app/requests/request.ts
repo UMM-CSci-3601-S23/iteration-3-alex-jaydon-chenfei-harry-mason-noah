35d3859fe90a1588728c1c8912f4e53cef4452ca
Noah Kilker <kilke023@umn.edu>
2023-04-21 13:44:41 -0500

    got rid of merge conflict issues in the files
    title

--- client/src/app/requests/request.ts
+++ client/src/app/requests/request.ts
@@ -2,13 +2,7 @@ export interface Request {
   _id: string;
   name: string;
   dateAdded: string;
-<<<<<<< HEAD
   description?: string;
-=======
-  itemType?: ItemType;
-  description?: string;
-  foodType?: FoodType;
->>>>>>> working-on-requestform
   selections?: string[];
   diaperSize?: string;
 }
