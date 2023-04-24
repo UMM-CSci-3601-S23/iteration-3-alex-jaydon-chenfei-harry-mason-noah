9eb8e5459ba3ed2c0def8ca90c823f8a5d850b5e
ChenfeiP <peng0368@morris.umn.edu>
2023-04-24 18:23:36 -0500

    Add new parametes(not required) to Request

--- client/src/app/requests/request.ts
+++ client/src/app/requests/request.ts
@@ -3,7 +3,10 @@ export interface Request {
   itemType: ItemType;
   description: string;
   foodType: FoodType;
+  comment?: string;
+  timeSlot?: TimeSlot;
 }
 
 export type ItemType = 'food' | 'toiletries' | 'other';
 export type FoodType = '' | 'dairy' | 'grain' | 'meat' | 'fruit' | 'vegetable';
+export type TimeSlot = '' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
