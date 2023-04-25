6761afee30d56dcb9284406f737a2895e5d6106c
Noah Kilker <kilke023@umn.edu>
2023-04-25 14:24:46 -0500

    Updated Volunteer View, fixed diaper bug
    Updated volunteer view to use request.service like donor view is
    Fixed a bug where diapers would always display their size as 'undefined'
    
    Co-authored-by: Mason Eischens <eisch115@morris.umn.edu>

--- client/src/app/requests/request-volunteer.component.ts
+++ client/src/app/requests/request-volunteer.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { Request } from './request';
 import { RequestService } from './request.service';
-
 
 
 @Component({
@@ -16,133 +13,16 @@ import { RequestService } from './request.service';
 })
 
 export class RequestVolunteerComponent implements OnInit, OnDestroy {
-  @Input() request: Request;
   public serverFilteredRequests: Request[];
   public filteredRequests: Request[];
   public requestDescription: string;
   public readableRequests: Request[];
 
-  readonly itemMap = new Map<string, string>([
-    ['glutenFree','Gluten Free'],
-    ['lowSugar','Low Sugar'],
-    ['lactoseFree','Lactose Free'],
-    ['vegetarian','Vegetarian'],
-    ['miscFreshFruit','Misc. fresh fruit'],
-    ['appleJuice','Apple juice'],
-    ['frozenPeaches','Frozen peaches'],
-    ['mixedFruit','Mixed fruit'],
-    ['peaches','Peaches'],
-    ['appleSauce','Apple sauce'],
-    ['dates','Dates'],
-    ['carrots','Carrots'],
-    ['potatoes','Potatoes'],
-    ['miscVeggies','Misc. veggies'],
-    ['corn','Corn'],
-    ['greenBeans','Green beans'],
-    ['peas','Peas'],
-    ['sweetPotatoes','Sweet potatoes'],
-    ['spinach','Spinach'],
-    ['cannedCarrots','Canned carrots'],
-    ['dicedTomatoes','Diced tomatoes'],
-    ['spaghettiSauce','Spaghetti sauce'],
-    ['groundBeef','Ground beef'],
-    ['groundPorkBeefBlend','Ground beef/pork blend'],
-    ['plantBasedBurgers','Plant based burgers'],
-    ['pizzaRanchPizza','Pizza ranch pizza'],
-    ['veggieRavioli','Veggie ravioli'],
-    ['chickenDrumsticks','Chicken drumsticks'],
-    ['wholeChicken','Whole chicken'],
-    ['chickenBreast','Chicken breast'],
-    ['chickenLegQtrs','Chicken leg quarters'],
-    ['fishSticks','Fish sticks'],
-    ['ham','Ham'],
-    ['assortedMeats','Assorted meats'],
-    ['chicken','Chicken'],
-    ['tuna','Tuna'],
-    ['salmon','Salmon'],
-    ['pastaWithMeatSauce','Pasta with meat sauce'],
-    ['pastaInButterSauce','Pasta in butter sauce'],
-    ['cannedChili','Canned chili'],
-    ['vegCurry','Veg curry'],
-    ['hotDogSauce','Hot dog sauce'],
-    ['blackEyedPeas','Black-eyed peas'],
-    ['yellowEyedBeans','Yellow-eyed beans'],
-    ['pintoBeans','Pinto beans'],
-    ['porkAndBeans','Pork and beans'],
-    ['blackBeans','Black beans'],
-    ['driedPintoBeans','Dried pinto beans'],
-    ['yellowSplitPeas','Yellow split peas'],
-    ['kidneyBeans','Kidney beans'],
-    ['miscDriedBeans','Misc. dried beans'],
-    ['peanutButter','Peanut butter'],
-    ['almonds','Almonds'],
-    ['walnuts','Walnuts'],
-    ['crackers','Crackers'],
-    ['cookies','Cookies'],
-    ['miscSnacks','Misc. snacks'],
-    ['refriedBeans','Refried beans'],
-    ['whiteBeans','White beans'],
-    ['rice','Rice'],
-    ['stuffingMix','Stuffing mix'],
-    ['pancakeMix','Pancake mix'],
-    ['quickOats','Quick oats'],
-    ['readyToEatCereal','Ready-to-eat cereal'],
-    ['elbowNoodles','Elbow noodles'],
-    ['macAndCheese','Mac and cheese'],
-    ['pennePasta','Penne pasta'],
-    ['instantPastaOrRice','Instant pasta or rice'],
-    ['bread','Bread'],
-    ['hamburgerBuns','Hamburger buns'],
-    ['hotDogBuns','Hotdog buns'],
-    ['bakedGoods','Baked goods'],
-    ['freshMilkLactoseFree','Fresh milk (lactose free)'],
-    ['miscDairyProducts','Misc. dairy products'],
-    ['cheese','Cheese'],
-    ['yogurt','Yogurt'],
-    ['butter','Butter'],
-    ['shelfStableMilk','Shelf stable milk'],
-    ['bakingMix','Baking mix'],
-    ['cakeMix','Cake mix'],
-    ['flour','Flour'],
-    ['muffinMix','Muffin mix '],
-    ['cookieMix','Cookie mix'],
-    ['miscBakingItems','Misc. baking items'],
-    ['vegOil','Vegetable oil'],
-    ['chickenNoodle','Chicken noodle soup'],
-    ['tomatoSoup','Tomato soup'],
-    ['vegetableSoup','Vegetable soup'],
-    ['creamyCannedSoup','Creamy canned soup'],
-    ['miscSoups','Misc. soups'],
-    ['seasonings','Seasonings'],
-    ['hotSauce','Hot sauce'],
-    ['saladDressing','Salad dressing'],
-    ['ranchDressing','Ranch dressing'],
-    ['mustard','Mustard'],
-    ['syrup','Syrup'],
-    ['miscPickled','Misc. pickled'],
-    ['fruitOrVegetablePuree','Fruit or vegetable purée'],
-    ['babyCereal','Baby cereal'],
-    ['formula','Formula'],
-    ['newbornGiftBag','Newborn gift bag'],
-    ['diapers','Diapers'],
-    ['shampoo','Shampoo'],
-    ['bodyOrHandSoap','Body or hand soap'],
-    ['toothpaste','Toothpaste'],
-    ['toothbrushes','Toothbrushes'],
-    ['birthdayPartyKit','Birthday party kit'],
-    ['handSanitizer','Hand sanitizers'],
-    ['feminineHygiene','Feminine hygiene'],
-    ['dishSoap','Dish soap'],
-    ['laundryDetergent','Laundry detergent'],
-    ['disinfectingWipes', 'Disinfecting wipes']
-  ]);
-
   authHypothesis: boolean;
 
   private ngUnsubscribe = new Subject<void>();
 
-
-  constructor(private requestService: RequestService, private snackBar: MatSnackBar) {
+  constructor(public requestService: RequestService, private snackBar: MatSnackBar) {
   }
   //Gets the requests from the server with the correct filters
   getRequestsFromServer(): void {
@@ -152,7 +32,6 @@ export class RequestVolunteerComponent implements OnInit, OnDestroy {
       takeUntil(this.ngUnsubscribe)
     ).subscribe({
       next: (returnedRequests) => {
-        this.makeRequestsReadable(returnedRequests);
         this.serverFilteredRequests = returnedRequests;
       },
 
@@ -165,29 +44,6 @@ export class RequestVolunteerComponent implements OnInit, OnDestroy {
     });
   }
 
-  public makeRequestsReadable(formList: Request[]){
-    const items = this.itemMap;
-
-    // eslint-disable-next-line @typescript-eslint/prefer-for-of
-    for (let i = 0; i < formList.length; i++){
-      const tempTime = formList[i].dateAdded;
-      if (tempTime.includes('submitted a form on:')){
-        formList[i].dateAdded = tempTime;
-      }else{
-        formList[i].dateAdded = 'submitted a form on: ' + tempTime.substring(4, 6) + '-' + tempTime.substring(6,8)
-      + '-'+ tempTime.substring(0, 4);
-      }
-      console.log(formList[i].dateAdded);
-      for (let ii = 0; ii < formList[i].selections.length; ii++){
-        if (formList[i].selections[ii] === 'diapers' && formList[i].diaperSize){
-          formList[i].selections[ii] = ' ' + items.get(formList[i].selections[ii]) + ' (size: ' + formList[i].diaperSize + ')';
-        }else {
-          formList[i].selections[ii] = ' ' + items.get(formList[i].selections[ii]);
-        }
-      }
-    }
-    this.readableRequests = formList;
-  }
   //
   public updateFilter(): void {
     this.filteredRequests = this.serverFilteredRequests;
@@ -217,18 +73,6 @@ export class RequestVolunteerComponent implements OnInit, OnDestroy {
           {duration: 5000});
       },
     });
-  }
-
-  public fixSelections(request: Request): void {
-    for (let i = 0; i < request.selections.length; i++){
-      const iterator1 = this.itemMap.entries();
-      for (let ii =0; ii < 112; ii++){
-        const valuePair = iterator1.next().value();
-        if (valuePair[1] === request.selections[i]){
-          request.selections[i] = valuePair[0];
-        }
-      }
-    }
   }
 
   public postRequest(request: Request): void {
