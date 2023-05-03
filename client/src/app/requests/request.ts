74d95b23c42e85e67a2a6d2749f13a75ffea10a7
Mason Eischens <eisch115@morris.umn.edu>
2023-05-02 19:44:34 -0500

    Expands list of required class parameters
    Co-authored-by: Noah Kilker <kilke023@umn.edu>
    Co-authored-by:  Chenfei Peng <peng0368@morris.umn.edu>

--- client/src/app/requests/request.ts
+++ client/src/app/requests/request.ts
@@ -1,12 +1,11 @@
 export interface Request {
   _id: string;
-  amount?: number;
   name: string;
   // in the future income will be required
   incomeValid: string;
   dateAdded: string;
   description?: string;
   selections?: string[];
-  fulfilled?: string[];
+  fulfilled: string[];
   diaperSize?: string;
 }
