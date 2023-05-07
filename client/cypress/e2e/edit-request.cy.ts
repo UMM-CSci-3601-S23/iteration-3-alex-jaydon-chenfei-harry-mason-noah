00eecfe2038a17379dba6ce709dc83ac43c3d158
ChenfeiP <peng0368@morris.umn.edu>
2023-05-07 14:47:56 -0500

    make progress in e2e with edit-requests

--- client/cypress/e2e/edit-request.cy.ts
+++ client/cypress/e2e/edit-request.cy.ts
@@ -7,26 +7,30 @@ describe('Edit a request', ()=> {
   beforeEach(()=> {
     cy.setCookie('auth_token', 'TOKEN');
     cy.task('seed:database');
-    page.navigateToRequest();
+    page.navigateToEditRequest();
   });
 
-  it('Should edit the request', ()=> {
-    const request: Request = {
-      _id: '588935f57546a2daea44de7c',
-      dateAdded: '20230423',
-      name: '',
-      incomeValid: '',
-      fulfilled: [],
-      priority: 0
-    };
-
-    page.navigateToDonor();
-
-    // page.selectItemType('food');
-    // page.selectFoodType('meat');
-    page.filterDescription('This is a test edit');
-
-    // cy.get('.donor-list-itemType').should('contain.text', request.itemType);
+  it('should save the changes after click the submitRevision', () => {
+    page.getFormField('clientName').clear().type('Alex');
+    page.getFormField('description').clear().type('For the purpose of testing');
+    page.submitRevision();
+    page.navigateToEditRequest();
+    page.getFormField('clientName').should('have.value', 'Alex');
+    page.getFormField('description').should('have.value', 'For the purpose of testing');
   });
 
+  it('should change the word color to green when mat-checkbox is checked', () => {
+    // Find the first mat-card-title and store its class
+    cy.get('.itemName').first().as('firstItemName');
+    // Check the color before checking the mat-checkbox (assuming it's red)
+    cy.get('@firstItemName')
+      .invoke('css', 'color')
+      .should('eq', 'rgb(255, 0, 0)'); // Red in RGB
+    // Find the first mat-checkbox and check it
+    cy.get('mat-checkbox').first().click();
+    // Check if the color of the mat-card-title changed to green
+    cy.get('@firstItemName')
+      .invoke('css', 'color')
+      .should('eq', 'rgb(0, 128, 0)'); // Green in RGB
+  });
 });
